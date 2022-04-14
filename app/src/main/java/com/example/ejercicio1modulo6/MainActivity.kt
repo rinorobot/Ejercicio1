eda69721691ecd8be19a38674daf65ffc4367041
rinorobot <sgomez_moya@hotmail.com>
2022-04-13 20:36:33 -0500

    Primer commit

---
+++ app/src/main/java/com/example/ejercicio1modulo6/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.ejercicio1modulo6
+
+import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+    }
+}
